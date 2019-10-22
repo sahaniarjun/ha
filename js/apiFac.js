fbc8082194d4f1d98d2af4874039691188878d1a
User <user@rba-hackerrank.com>
2019-10-22 19:04:25 +0000

    Auto save changes
    Commit by HackerRank

--- js/apiFac.js
+++ js/apiFac.js
@@ -2,7 +2,15 @@ angular.module('dictionaryApp')
   .factory('apiFac',function($http) {
     var apiFac;
     apiFac.synonym = function(word){
+        apiFac.synonym = function(word){
+        $http
+            .get("https://api.wordnik.com/v4/word.json/"+word+"/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=c23b746d074135dc9500c0a61300a3cb7647e53ec2b9b658e")
+            .then(function(response){
+                return response
+            })
       return ;
+    };
+      return "dfd";
     };
     return apiFac;
   });
--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 var app = angular.module("dictionaryApp", []);
 app.controller("dictionaryController", function ($scope, apiFac,$compile) {
-    $scope.get_content;
+    $scope.result_word="nothing";
+    $scope.get_content=function(){
+        console.log("called")
+    };
     $scope.search ;
+    $scope.items=["hey"];
 
 });
